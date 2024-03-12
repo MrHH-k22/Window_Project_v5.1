4c0da3dce6c297365158e60eeb9764199bb35969
TinTranT <trantrungtin254@gmail.com>
2024-03-12 17:58:26 +0700

    Create FInformation

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace Window_Project_v5._1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap user {
+            get {
+                object obj = ResourceManager.GetObject("user", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap web_analytics {
             get {
                 object obj = ResourceManager.GetObject("web-analytics", resourceCulture);
