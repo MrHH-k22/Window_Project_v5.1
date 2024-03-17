27cc4d95b6286d640c8705221d33371f3fb1eb67
MrHH-k22 <nguyenmaihuyhoang156@gmail.com>
2024-03-17 15:05:18 +0700

    add Util class and add contructors

--- Account.cs
+++ Account.cs
@@ -16,8 +16,30 @@ namespace Window_Project_v5._1
         private DateTime Birthday;
         private string address;
         private byte[] avatar;
+
+        public Account()
+        {
+        }
+
+        public Account(int id, string email, string password, string name, string phone, DateTime birthday, string address, byte[] avatar)
+        {
+            this.id = id;
+            this.email = email;
+            this.password = password;
+            this.name = name;
+            Phone = phone;
+            Birthday = birthday;
+            this.address = address;
+            this.avatar = avatar;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string Name { get => name; set => name = value; }
+        public string Phone1 { get => Phone; set => Phone = value; }
+        public DateTime Birthday1 { get => Birthday; set => Birthday = value; }
+        public string Address { get => address; set => address = value; }
+        public byte[] Avatar { get => avatar; set => avatar = value; }
     }
-
-
-
 }
--- Product.cs
+++ Product.cs
@@ -14,9 +14,33 @@ namespace Window_Project_v5._1
         private double salePrice;
         private string condition;
         private string status;
-        private List<byte[]> Images;
+        private List<byte[]> images;
 
+        public Product()
+        {
+        }
+
+        public Product(int id, string name, double originalPrice, double salePrice, string condition, string status, List<byte[]> images)
+        {
+            this.id = id;
+            this.name = name;
+            this.originalPrice = originalPrice;
+            this.salePrice = salePrice;
+            this.condition = condition;
+            this.status = status;
+            this.images = images;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public double OriginalPrice { get => originalPrice; set => originalPrice = value; }
+        public double SalePrice { get => salePrice; set => salePrice = value; }
+        public string Condition { get => condition; set => condition = value; }
+        public string Status { get => status; set => status = value; }
+        public List<byte[]> Images { get => images; set => images = value; }
     }
+
+
 
 
 }
