30f2b4ada3679650767a110f8cde4d4e69506774
MrHH-k22 <nguyenmaihuyhoang156@gmail.com>
2024-03-17 15:08:02 +0700

    fix attributes

--- Account.cs
+++ Account.cs
@@ -12,8 +12,8 @@ namespace Window_Project_v5._1
         private string email;
         private string password;
         private string name;
-        private string Phone;
-        private DateTime Birthday;
+        private string phone;
+        private DateTime birthday;
         private string address;
         private byte[] avatar;
 
@@ -27,8 +27,8 @@ namespace Window_Project_v5._1
             this.email = email;
             this.password = password;
             this.name = name;
-            Phone = phone;
-            Birthday = birthday;
+            this.phone = phone;
+            this.birthday = birthday;
             this.address = address;
             this.avatar = avatar;
         }
@@ -37,8 +37,8 @@ namespace Window_Project_v5._1
         public string Email { get => email; set => email = value; }
         public string Password { get => password; set => password = value; }
         public string Name { get => name; set => name = value; }
-        public string Phone1 { get => Phone; set => Phone = value; }
-        public DateTime Birthday1 { get => Birthday; set => Birthday = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public DateTime Birthday { get => birthday; set => birthday = value; }
         public string Address { get => address; set => address = value; }
         public byte[] Avatar { get => avatar; set => avatar = value; }
     }
