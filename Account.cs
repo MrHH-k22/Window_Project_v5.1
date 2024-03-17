e6256620dc1f020a01e3e1f556f4c2f4b9a1e130
MrHH-k22 <nguyenmaihuyhoang156@gmail.com>
2024-03-17 14:58:34 +0700

    add Product, account, DBconnection classes

---
+++ Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Window_Project_v5._1
+{
+    internal class Account
+    {
+        private int id;
+        private string email;
+        private string password;
+        private string name;
+        private string Phone;
+        private DateTime Birthday;
+        private string address;
+        private byte[] avatar;
+    }
+
+
+
+}
