cdada78fc2bae211d455def098affccf114b8959
Nguyen Nhat An <ngnhatan1205@gmail.com>
2024-04-02 21:41:15 +0700

    FForget password

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace Window_Project_v5._1.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Window_Project;Integrated Secu" +
-            "rity=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Window_Final_Project;Integrate" +
+            "d Security=True;")]
         public string connStr {
             get {
                 return ((string)(this["connStr"]));
