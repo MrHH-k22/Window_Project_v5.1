1d7acd5edd563a10c751950abb6f98997673fe0c
Nguyen Nhat An <ngnhatan1205@gmail.com>
2024-04-03 17:03:53 +0700

    ...

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -213,6 +213,16 @@ namespace Window_Project_v5._1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap forgotpw1 {
+            get {
+                object obj = ResourceManager.GetObject("forgotpw1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap HAN00112_Lớn_1024x682 {
             get {
                 object obj = ResourceManager.GetObject("HAN00112-Lớn-1024x682", resourceCulture);
@@ -406,6 +416,16 @@ namespace Window_Project_v5._1.Properties {
         internal static System.Drawing.Bitmap MessageWarning {
             get {
                 object obj = ResourceManager.GetObject("MessageWarning", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap png_transparent_computer_icons_refresh_text_monochrome_black_thumbnail {
+            get {
+                object obj = ResourceManager.GetObject("png-transparent-computer-icons-refresh-text-monochrome-black-thumbnail", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
