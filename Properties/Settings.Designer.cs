5fba06cf12641dbe635367c26d608c7c12758f6e
MrHH-k22 <nguyenmaihuyhoang156@gmail.com>
2024-04-21 19:50:39 +0700

    nothing

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace Window_Project_v5._1.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-83OL787\\SQLEXPRESS;Initial Catalog=Shopping;Integrated Securi" +
-            "ty=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=WindowFinalProject11;Integrate" +
+            "d Security=True")]
         public string connStr {
             get {
                 return ((string)(this["connStr"]));
